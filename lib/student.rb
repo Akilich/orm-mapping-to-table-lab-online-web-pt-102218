29876aed134c6a6a646673cabe68b484178584f6
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-30 06:13:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,9 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-  def initialize(name, grade, id)
+  def initialize(name, grade)
     @name=name
     @grade=grade
-    @id=id
   end
 
  def self.create_table
