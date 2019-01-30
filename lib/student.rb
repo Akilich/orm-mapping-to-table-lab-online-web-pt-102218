c614a3dd5ffcbe377fda462414d3ea9ea2a9e1e1
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-30 06:16:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,6 +32,7 @@ attr_reader :id
       VALUES (?, ?)
     SQL
   DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]
   end
   
   def self.create(name:, grade:)
